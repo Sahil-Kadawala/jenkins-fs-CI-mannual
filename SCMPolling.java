86e1c8216939bbb3a815103fc83e6359f4b6c160
sahilk <sahil01130@gmail.com>
2026-02-06 22:46:35 +0530

    changes in SCMPolling.java

--- SCMPolling.java
+++ SCMPolling.java
@@ -1,5 +1,6 @@
 class SCMPolling {
 	public static void main(String args[]){
-		System.out.println("SCM Pollinggggggg"); 
+		System.out.println("SCM Pollinggggggg");
+		System.out.println("heloooooo"); 
 	}
 }
