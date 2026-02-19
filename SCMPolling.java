1fb096dc31e76da30e8b95b19a0e12d1c67158c2
Sahil Kadawala <155080067+Sahil-Kadawala@users.noreply.github.com>
2026-02-20 02:07:31 +0530

    Add Pipeline-1 output to SCMPolling

--- SCMPolling.java
+++ SCMPolling.java
@@ -2,5 +2,6 @@ class SCMPolling {
 	public static void main(String args[]){
 		System.out.println("SCM Pollinggggggg");
 		System.out.println("heloooooo"); 
+		System.out.println("Pipeline-1");
 	}
 }
